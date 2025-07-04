9d3dad75c43b24388c3c4c0ec1bcb7f7527b2c7c
aragakerubo <aragakerubo@gmail.com>
2025-07-03 20:15:58 -0500

    Flask if-for template structure

--- flask-03-handling-routes-and-if-for/flask_If_for_structure/app.py
+++ flask-03-handling-routes-and-if-for/flask_If_for_structure/app.py
@@ -1,19 +1,27 @@
 # Import Flask modules
+from flask import Flask, render_template
+
+# Create an object named app
+app = Flask(__name__)
 
 
-# Create an object named app 
-
-
-
-# Create a function named head which shows the massage as "This is my first conditions experience" in `index.html` 
+# Create a function named head which shows the massage as "This is my first conditions experience" in `index.html`
 # and assign to the route of ('/')
+@app.route("/")
+def head():
+    message = "This is my first conditions experience"
+    # return message
+    return render_template("index.html", message=message)
 
 
-
-# Create a function named header which prints numbers elements of list one by one in `index.html` 
-# and assign to the route of ('/')
-
+# Create a function named header which prints numbers elements of list one by one in `body.html`
+# and assign to the route of ('/list')
+@app.route("/list")
+def header():
+    names = ["Jane", "Loius", "Ida", "Kwame", "Sara"]
+    return render_template("body.html", object=names)
 
 
 # run this app in debug mode on your local.
-
+if __name__ == "main":
+    app.run(debug=True)
